72ae46780049aa83a29d0ef5b7b4dc3677ee7346
mark <markpackham1@gmail.com>
2022-01-26 12:22:09 +0000

    create NuGet package via terminal
    https://youtu.be/f8JyT6J4b1Q?t=147

---
+++ AppLogger2/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppLogger2
+{
+      public class Logger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}

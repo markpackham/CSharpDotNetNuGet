27ab91294f3436bc7e976a70d8247a04a4a8ab40
mark <markpackham1@gmail.com>
2022-01-26 10:55:36 +0000

    Learn NuGet from https://www.youtube.com/watch?v=ohaz_sPLp4Y&list=PLdo4fOcmZ0oVLvfkFk8O9h6v2Dcdh2bh_&index=2

---
+++ LearnNuGet/NugetExample/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(TimeSpan.FromMilliseconds(1299630020));
+Console.WriteLine(TimeSpan.FromMilliseconds(3603001));
+Console.WriteLine(TimeSpan.FromMilliseconds(8157019263));

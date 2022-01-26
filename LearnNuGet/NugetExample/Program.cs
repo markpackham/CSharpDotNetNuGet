f154f75a4ce2219792857f98c89c276d7720f006
mark <markpackham1@gmail.com>
2022-01-26 11:07:25 +0000

    use Humanizer for human friendly date times
    https://www.youtube.com/watch?v=oM-G7un2GkI&list=PLdo4fOcmZ0oVLvfkFk8O9h6v2Dcdh2bh_&index=3

--- LearnNuGet/NugetExample/Program.cs
+++ LearnNuGet/NugetExample/Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(TimeSpan.FromMilliseconds(1299630020));
-Console.WriteLine(TimeSpan.FromMilliseconds(3603001));
-Console.WriteLine(TimeSpan.FromMilliseconds(8157019263));
+﻿using Humanizer;
+
+Console.WriteLine(TimeSpan.FromMilliseconds(1299630020).Humanize(3));
+Console.WriteLine(TimeSpan.FromMilliseconds(3603001).Humanize(3));
+Console.WriteLine(TimeSpan.FromMilliseconds(8157019263).Humanize(3));
+
+// 2 weeks, 1 day, 1 hour
+// 1 hour, 3 seconds, 1 millisecond
+// 13 weeks, 3 days, 9 hours

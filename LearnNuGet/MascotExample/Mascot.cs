d753291bbb8df4d344a26814cd10014755a59d0b
mark <markpackham1@gmail.com>
2022-01-26 11:20:04 +0000

    add MascotExample
    https://youtu.be/oM-G7un2GkI?t=104

---
+++ LearnNuGet/MascotExample/Mascot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetExample
+{
+    public class Mascot
+    {
+        public String name { get; set; }
+        public String team { get; set; }
+        public String catchPhrase { get; set; }
+    }
+}
---
+++ LearnNuGet/MascotExample/Program.cs
@@ -0,0 +1,8 @@
+﻿using NugetExample;
+
+Mascot nugetMascot = new Mascot()
+    {
+        name = "NuGet Warior",
+        team = "NuGet",
+        catchPhrase = "Where packages come to life!"
+    };
